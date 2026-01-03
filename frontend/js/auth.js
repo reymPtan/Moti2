d8b136dcaedc9e01957cb9a05a03a1f56772725b
reymPtan <rpdereche@antiquespride.edu.ph>
2026-01-03 18:21:28 +0800

    Add files via upload

---
+++ frontend/js/auth.js
@@ -0,0 +1,3 @@
+function login(){alert("Demo login");location.href='dashboard.html';}
+function register(){alert("Demo register");location.href='index.html';}
+function logout(){location.href='index.html';}
